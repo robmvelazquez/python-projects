a525d95ca376d79516f54a3668ff7c72f6bb0408
Robert Velazquez <107489923+robmvelazquez@users.noreply.github.com>
2023-05-18 16:26:29 -0700

    create a multiple choice test

---
+++ multiple_question_test/Question.py
@@ -0,0 +1,5 @@
+# Creates the Question class that will contain the prompt and answer for our function.
+class Question:
+    def __init__(self, prompt, answer):
+        self.prompt = prompt
+        self.answer = answer
