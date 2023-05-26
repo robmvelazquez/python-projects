7000d87a092fd48c0a03cf0f0a98bb43f4e2b88f
Robert Velazquez <robmvelazquez@gmail.comgh auth logingit config --global user.email robmvelazquez@gmail.com>
2023-05-26 01:21:45 -0700

    updated test file from .txt to .py

---
+++ testfile.py
@@ -0,0 +1,4 @@
+# this file has been used to test the commit and push functionality between PyCharm and GitHub.
+# this file will be used for further miscellaneous tests down the line.
+mylist = ["banana", "cherry", "apple", "apple",   1, True]
+print(mylist)
