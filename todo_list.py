6fc164cdcc4484491627541f506f5aa0e598515a
Robert Velazquez <robmvelazquez@gmail.comgh auth logingit config --global user.email robmvelazquez@gmail.com>
2023-05-28 15:11:41 -0700

    added a display menu function

--- todo_list.py
+++ todo_list.py
@@ -1,10 +1,24 @@
-my_list = []
+# This function will be used to display the menu of our application.
+def display_menu():
+    print(" --- Objectives List Menu --- ")
+    print("1. Add a task")
+    print("2. View tasks")
+    print("3. Mark task as completed")
+    print("4. Remove completed Tasks")
+    print("5. Exit")
 
-# Prompts for user input
-user_input = input("Enter a task description: ")
-while user_input != 'done':
-    my_list.append(user_input)
-    user_input = input("Enter another item (or 'done' when finished): ")
+# This function will add a task into the Todo list.
+def add_task(todo_list):
+    task = input("Enter a task: ")
+    todo_list.append(task)
+    print("Task added successfully.")
 
-# Prints the final list
-print("Final list: ", my_list)
+# This function will be used to view tasks.
+def view_tasks(todo_list):
+    print(" === Tasks === ")
+    if len(todo_list) == 0:
+        print("No tasks found.")
+    else:
+        for index, task in enumerate(todo_list, start=1):
+            print(f"{index}. {task}")
+
