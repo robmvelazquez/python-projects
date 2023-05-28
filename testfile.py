01b910bffddcbdc4906ce1c9377053ee2e9e61d3
Robert Velazquez <robmvelazquez@gmail.comgh auth logingit config --global user.email robmvelazquez@gmail.com>
2023-05-28 14:46:39 -0700

    concatenation practice

--- testfile.py
+++ testfile.py
@@ -1,4 +1,21 @@
 # this file has been used to test the commit and push functionality between PyCharm and GitHub.
 # this file will be used for further miscellaneous tests down the line.
-mylist = ["banana", "cherry", "apple", "apple",   1, True]
-print(mylist)
+from collections import namedtuple
+
+# These are examples of basic lists.
+basic_list1 = [1, 2, 3, 4, 5]
+basic_list2 = [1, "Two", 3, "Four", 5]
+integers = "This is a list with only integers: "
+str_and_int = "This is a list with integers and strings: "
+
+# To print the lists with a string, they must be concatenated.
+# This can be accomplished with the .join method.
+concatenated = integers + " ".join([str(item) for item in basic_list1])
+concatenated2 = str_and_int + " ".join([str(item) for item in basic_list2])
+print(concatenated)
+print(concatenated2)
+
+
+# Point = namedtuple('Point', 'x,y')
+# pt = Point(1, -4)
+# print(pt.x, pt.y)
