0fc239661fb811f785cd63ae7e735d1cd294e2f6
Robert Velazquez <robmvelazquez@gmail.comgh auth logingit config --global user.email robmvelazquez@gmail.com>
2023-05-28 17:08:13 -0700

    testing the use of permutations

--- testfile.py
+++ testfile.py
@@ -1,6 +1,10 @@
+import collections
+from collections import namedtuple
+import itertools
+
+
 # this file has been used to test the commit and push functionality between PyCharm and GitHub.
 # this file will be used for further miscellaneous tests down the line.
-from collections import namedtuple
 
 # These are examples of basic lists.
 basic_list1 = [1, 2, 3, 4, 5]
@@ -15,13 +19,32 @@ concatenated2 = str_and_int + " ".join([str(item) for item in basic_list2])
 print(concatenated)
 print(concatenated2)
 
+# Each time an iterator is called, it returns the next element
 print("\nThis is a basic iterable.")
-my_list1 = [1, 2, 3, 4, 5]
+my_list1 = [1, 2, 3, 4]
 my_iterator = iter(my_list1)
 print(next(my_iterator))
 print(next(my_iterator))
 print(next(my_iterator))
 
-# Point = namedtuple('Point', 'x,y')
-# pt = Point(1, -4)
-# print(pt.x, pt.y)
+# namedtuple is a function from the collections module
+print("\nThis is an example of a namedtuple")
+Point = namedtuple('Point', 'x,y')
+pt = Point(1, -4)
+print(pt.x, pt.y)
+
+# permutations output all possible arrangements of elements within an iterable.
+# permutations is a function from the itertools module.
+permutations = itertools.permutations(my_list1)
+print("\nThis is an example of permutations")
+for perm in permutations:
+    print(perm)
+
+# combinations output all possible combinations of elements within an iterable.
+# combinations requires two arguments: the iterable and the length.
+comb = itertools.combinations(my_list1, 2)
+print("\nThis is an example of combinations")
+print(list(comb))
+
+# combinations with replacements
+comb_wr = itertools.combinations_with_replacement(my_list1, 2)
