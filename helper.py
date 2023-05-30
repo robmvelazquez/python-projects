08bb0297649c9571513e667dc876e98cf9997117
Robert Velazquez <robmvelazquez@gmail.com>
2023-05-30 16:06:45 -0700

    utilizing logger handlers

--- helper.py
+++ helper.py
@@ -1,3 +1,27 @@
 import logging
+
+"""
 logger = logging.getLogger(__name__)
+# logger.propagate = False will stop the logger from propagating to the parent logger/handlers.
 logger.info('hello from helper')
+"""
+
+logger = logging.getLogger(__name__)
+
+# Create Handler
+stream_h = logging.StreamHandler()
+file_h = logging.FileHandler('file.log')
+
+# Level and Format
+stream_h.setLevel(logging.WARNING)
+file_h.setLevel(logging.ERROR)
+
+formatter = logging.Formatter('(%name)s - %(levelname)s - %(message)s')
+stream_h.setFormatter(formatter)
+file_h.setFormatter(formatter)
+
+logger.addHandler(stream_h)
+logger.addHandler(file_h)
+
+logger.warning('This is a warning.')
+logger.error('This is an error.')
