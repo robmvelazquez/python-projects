57ef45f0525f658cc0537776000b346f8b78ab77
Robert Velazquez <robmvelazquez@gmail.com>
2023-05-31 13:51:12 -0700

    utilizing logging functionalities

--- learning/logging.py
+++ learning/logging.py
@@ -1,6 +1,5 @@
 import logging
 
-
 """
 Example 1 : All messages will display the time and date in the format as specified.
 
@@ -14,14 +13,12 @@ Example 1 : All messages will display the time and date in the format as specifi
     logging.error('This is an error message')
     logging.critical('This is a critical message')
 
-
 Example 2 : 
 
     # Use this to display a message.
     logger = logging.getLogger(__name__)
     # logger.propagate = False will stop the logger from propagating to the parent logger/handlers.
     logger.info('hello from helper')
-
 
 Example 3 : The stream handler logs all messages of level 'WARNING' and above. In this scenario, warning
                 and error are output in the terminal. The file handler outputs the message of level 'ERROR'
@@ -47,13 +44,53 @@ Example 3 : The stream handler logs all messages of level 'WARNING' and above. I
     logger.warning('This is a warning.')
     logger.error('This is an error.')
 
-    Example 4 :
+Example 4 : Utilize these lines of code with the logging.conf file.
 
-"""
+    import logging.config   
 
+    logging.config.fileConfig('logging.conf')
 
+    logger = logging.getLogger('simpleExample')
+    logger.debug('This is a debug message')
 
-""" 
+Example 5 : This can be utilized to monitor errors.
+
+    try:
+        a = [1,2,3]
+        val = a[4]
+    except: IndexError as e:
+        logging.error(e, exc_info=True)
+
+Example 6 : Rotating File Handler will output the INFO into a log file until reaching 2MB. When 2 MB is reached,
+            a new log file will be generated, with a maximum of 5 log files at a time.
+
+    import logging
+    from logging.handlers import RotatingFileHandler
+
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+
+    handler = RotatingFileHandler('app.log', maxBytes=2000, backupCount=5)
+    logger.addHandler(handler)
+
+    for _ in range(10000):
+        logger.info('Hello, World!')
+
+Example 7: Timed Rotating File Handler will output a rotating log file handler based on how much time has passed.
+    
+    import logging
+    from logging.handlers import TimedRotatingFileHandler
+
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+
+    handler = TimedRotatingFileHandler('timed_test_log', when='s', interval=5, backupCount=5)
+    logger.addHandler(handler)
+
+    for _ in range(6):
+        logger.info('Hello, World!')
+        time.sleep(5)
+
 ===== LOGGERS ===== 
 - named entities that provide an interface to interact with the logging framework.
  
