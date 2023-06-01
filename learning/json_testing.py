dd9531c13ead823b544393c2a59e03f79de3ddc3
Robert Velazquez <robmvelazquez@gmail.com>
2023-05-31 17:16:07 -0700

    syntax edits

--- learning/json_testing.py
+++ learning/json_testing.py
@@ -2,7 +2,7 @@ import json
 
 # Convert Python dict to JSON object
 
-
+person = {"name": "John", "age": 30, "city": "New York", "hasChildren": False, "titles": ["engineer", "programmer"]}
 
 # The sort_keys method will sort the Python dict into an alphabetical JSON object. The 's' in dump(s) means 'str'.
 personJSON = json.dumps(person, indent=4, sort_keys=True)
@@ -13,7 +13,9 @@ print(personJSON)
 """
 with open('person.json', 'w') as file:
     json.dump(person, file, indent=4)
-"""
+
+# Read JSON data within JSON file:
 with open('person.json', 'r') as file:
     person = json.load(file)
     print(person)
+"""
