22e53771f37511c9bfd45c9e21b6521db0f5cec3
Robert Velazquez <robmvelazquez@gmail.com>
2023-06-06 16:07:53 -0700

    added comments and refactored file name

--- learning/threading_and_processing.py
+++ learning/threading_and_processing.py
@@ -9,11 +9,12 @@ def square_numbers():
         i * i
         time.sleep(0.1)
 
+# Define multiple (10) threads
 if __name__ == "__main__":
     threads = []
     num_threads = 10
 
-# Create threads
+# Create threads and define target (square_numbers)
 for i in range(num_threads):
     t = Thread(target=square_numbers)
     threads.append(t)
@@ -22,7 +23,7 @@ for i in range(num_threads):
 for thread in threads:
     thread.start()
 
-# Join processes
+# Join = wait for threads to finish execution before printing 'End main'.
 for thread in threads:
     thread.join()
 
