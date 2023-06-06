473a5c99a09d34187eb3d1427867df41221f406c
Robert Velazquez <robmvelazquez@gmail.com>
2023-06-06 16:26:53 -0700

    acquires a lock to execute three threads

---
+++ learning/multi_threading.py
@@ -0,0 +1,36 @@
+from threading import Thread, Lock
+import time
+
+# Define global variable to simulate database
+database_value = 0
+
+def increase(lock):
+    global database_value
+
+    with lock:
+        local_copy = database_value
+        local_copy += 1
+        time.sleep(0.1)
+        database_value = local_copy
+
+
+if __name__ == "__main__":
+
+    lock = Lock()
+    print('start value', database_value)
+
+    thread1 = Thread(target=increase, args=(lock,))
+    thread2 = Thread(target=increase, args=(lock,))
+    thread3 = Thread(target=increase, args=(lock,))
+
+    thread1.start()
+    thread2.start()
+    thread3.start()
+
+    thread1.join()
+    thread2.join()
+    thread3.join()
+
+    print('end value', database_value)
+
+    print('end main')
