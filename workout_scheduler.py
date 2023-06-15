3f844da1d2e3d80bf401ee7d04bffa252087b306
Robert Velazquez <robmvelazquez@gmail.com>
2023-06-14 21:56:51 -0700

    modified the print statement in the create workout function

--- workout_scheduler.py
+++ workout_scheduler.py
@@ -12,7 +12,7 @@ thursday = []
 friday = []
 saturday = []
 sunday = []
-
+workout = ""
 
 def display_menu():
     print('1. View Today\'s Workout.')
@@ -42,6 +42,7 @@ def todays_workout(workout_scheduler):
 
 # This function allows the user to create a workout routine that will be saved for a specific day of the week
 def create_workout(workout_scheduler):
+    global workout
     for _ in days:
         print(_)
     day_input = input('Select a day of the week that you would like to ')
@@ -49,61 +50,88 @@ def create_workout(workout_scheduler):
         day = int(day_input)
         if 1 <= day <= 7:
             if day == 1:
-                workout = input('Enter a name for the workout you will perform on Monday: ')
+                enter_workout(workout_scheduler)
                 monday.append(workout)
-                print('You entered', workout)
+                print('You entered', workout, 'as the workout you would like to perform.')
             elif day == 2:
                 workout = input('Enter a name for the workout you will perform on' + day_name + ': ')
                 tuesday.append(workout)
-                print('You entered', workout)
+                print('You entered', workout, 'as the workout you would like to perform.')
             elif day == 3:
                 workout = input('Enter a name for the workout you will perform on Sunday: ')
                 wednesday.append(workout)
-                print('You entered', workout)
+                print('You entered', workout, 'as the workout you would like to perform.')
             elif day == 4:
                 workout = input('Enter a name for the workout you will perform on Sunday: ')
                 thursday.append(workout)
-                print('You entered', workout)
+                print('You entered', workout, 'as the workout you would like to perform.')
             elif day == 5:
                 workout = input('Enter a name for the workout you will perform on ' + day_name + ': ')
                 friday.append(workout)
-                print('You entered', workout)
+                print('You entered', workout, 'as the workout you would like to perform.')
             elif day == 6:
                 workout = input('Enter a name for the workout you will perform on Sunday: ')
                 saturday.append(workout)
-                print('You entered', workout)
+                print('You entered', workout, 'as the workout you would like to perform.')
             elif day == 7:
                 workout = input('Enter a name for the workout you will perform on Sunday: ')
                 sunday.append(workout)
-                print('You entered', workout)
+                print('You entered', workout, 'as the workout you would like to perform.')
             else:
                 print('Invalid entry, try again')
     except ValueError:
         print('Invalid input. Please enter a valid integer.')
 
 
+def enter_workout(workout_scheduler):
+    global workout
+    workout = input('Enter a name for the workout you will perform on Sunday: ')
+
+
 # This function allows the user to edit previous workout routines.
 def edit_workout(workout_scheduler):
+    global workout
     for _ in days:
         print(_)
-    day_input = input('Select a day of the week that you would like to ')
+    day_input = input('Select a day of the week that you would like to edit: ')
     try:
         day = int(day_input)
         if 1 <= day <= 7:
             if day == 1:
-
+                monday.remove(workout)
+                workout = input('Enter a name for the workout you will perform on Monday: ')
+                monday.append(workout)
+                print('You entered', workout)
             elif day == 2:
-
+                tuesday.remove(workout)
+                workout = input('Enter a name for the workout you will perform on' + day_name + ': ')
+                tuesday.append(workout)
+                print('You entered', workout)
             elif day == 3:
-
+                wednesday.remove(workout)
+                workout = input('Enter a name for the workout you will perform on Sunday: ')
+                wednesday.append(workout)
+                print('You entered', workout)
             elif day == 4:
-
+                thursday.remove(workout)
+                workout = input('Enter a name for the workout you will perform on Sunday: ')
+                thursday.append(workout)
+                print('You entered', workout)
             elif day == 5:
-
+                friday.remove(workout)
+                workout = input('Enter a name for the workout you will perform on ' + day_name + ': ')
+                friday.append(workout)
+                print('You entered', workout)
             elif day == 6:
-
+                saturday.remove(workout)
+                workout = input('Enter a name for the workout you will perform on Sunday: ')
+                saturday.append(workout)
+                print('You entered', workout)
             elif day == 7:
-
+                sunday.remove(workout)
+                workout = input('Enter a name for the workout you will perform on Sunday: ')
+                sunday.append(workout)
+                print('You entered', workout)
             else:
                 print('Invalid entry, try again')
     except ValueError:
