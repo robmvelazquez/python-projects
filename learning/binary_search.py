26a76bde572c1bc2eeea03c40f57a26aacc9b46a
Robert Velazquez <robmvelazquez@gmail.com>
2023-06-17 19:02:31 -0700

    created a binary search function

--- learning/binary_search.py
+++ learning/binary_search.py
@@ -1,3 +1,9 @@
+"""
+This function returns the index position of the target if found, else returns None.
+Binary search divides the search space by 2 repeatedly until the target value is found or determined to be absent.
+This function has a Big O value of O(log n)
+The list or array MUST be sorted in a sequential, ascending order for a successful binary search.
+"""
 
 
 def binary_search(list, target):
@@ -15,3 +21,18 @@ def binary_search(list, target):
             last = midpoint - 1
 
     return None
+
+
+# This function verifies if the element was found and prints the index the element was found.
+def verify(index):
+    if index is not None:
+        print('Target found at index: ', index)
+    else:
+        print('Target not found in list.')
+
+
+# This variable defines the elements within the list 'numbers'
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+result = binary_search(numbers, 6)
+verify(result)
