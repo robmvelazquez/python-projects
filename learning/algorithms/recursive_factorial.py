1db321f93f47ad8d7f78f7e38e90b74e42999fd7
Robert Velazquez <107489923+robmvelazquez@users.noreply.github.com>
2023-06-22 09:02:43 -0700

    added comments

--- learning/algorithms/recursive_factorial.py
+++ learning/algorithms/recursive_factorial.py
@@ -1,10 +1,14 @@
-# Recursive factorial function finds the factorial of an integer.
+"""
+Recursive factorial function finds the factorial of an integer. Recursive functions develop 
+clean code and break down a complex task into simple sub-problems. However, recursive calls are
+inefficient and difficult to debug.
+"""
 
 
 def factorial(x):
 
   if x == 1:
-      return 1
+      return 1  # This is the base condition which ends the function.
   else:
       return (x * factorial(x-1))
 
